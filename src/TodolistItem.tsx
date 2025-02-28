d8867255234a276546c1e2dc3a9a4db778e0b6d6
hrsk <hrsk.yegor@gmail.com>
2025-02-28 10:19:04 +0300

    named exports, typing and destructuring props

---
+++ src/TodolistItem.tsx
@@ -0,0 +1,32 @@
+
+type Props = {
+    title: string
+}
+
+export const TodolistItem = ({title}: Props) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={true}/> <span>JS</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={false}/> <span>React</span>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
